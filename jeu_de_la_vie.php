d1fe48d6b0963add5affb717d38f45185ff8340b
Thibault Roussel <thibaultroussel@MacBook-Pro-de-Thibault-2.local>
2020-11-18 11:19:34 +0100

    Ajout d'afficherTableau

--- jeu_de_la_vie.php
+++ jeu_de_la_vie.php
@@ -1,28 +1,64 @@
 <?php
-  $plateau = [
-    [ 0, 0, 0, 0, 0 ],
-    [ 0, 1, 1, 0, 0 ],
-    [ 0, 0, 1, 0, 0 ],
-    [ 0, 0, 0, 0, 0 ],
-    [ 0, 0, 0, 0, 0 ],
-  ];
+  $plateau = array(
+//         0  1  2  3  4
+    array( 0, 0, 0, 0, 0 ), // 0
+    array( 0, 1, 1, 0, 0 ), // 1
+    array( 0, 0, 1, 0, 0 ), // 2
+    array( 0, 0, 0, 0, 0 ), // 3
+    array( 0, 0, 0, 0, 0 )  // 4
+  );
 
-  $plateau2 = [
-    [ 0, 0, 0, 0, 0 ],
-    [ 0, 1, 1, 0, 0 ],
-    [ 0, 1, 1, 0, 0 ],
-    [ 0, 0, 0, 0, 0 ],
-    [ 0, 0, 0, 0, 0 ],
-  ]
+
+
 
   // https://fr.wikipedia.org/wiki/Jeu_de_la_vie
 
   // premier exercice : afficher un plateau de jeu (afficher un tableau à 2 dimensions)
 
+  function afficherTableau ($plateauAAfficher) {
+    // pour chaque élément de $plateauAAfficher que j'appelle $ligne
+    foreach($plateauAAfficher as $ligne) {
+    // pour chaque élément de $ligne que j'appelle $cellule
+      foreach($ligne as $cellule) {
+        print_r($cellule);
+        echo ' ';
+      }
+      echo '<br>';
+    }
+  }
+
+  echo 'Affichage du tableau $plateau <br>';
+  afficherTableau($plateau);
+
+
+
+
   // deuxième exercice : écrire une fonction qui pour une position donnée et un tableau donné
   // retourne le nombre de voisin vivants
+
+  // pour la cellule colonne 3, ligne 2, les voisins ont pour coordonnées : 
+
+  // [
+  //   [(2 , 1), (3 , 1), (4 , 1)],
+  //   [(2 , 2), (3 , 2), (4 , 2)],
+  //   [(2 , 3), (3 , 3), (4 , 3)],
+  // ]
+
+  // $tableau_exemple = [
+  //   [ (numColonne - 1, numLigne - 1), (numColonne, numLigne - 1), (numColonne + 1, numLigne - 1) ],
+  //   [ (numColonne - 1, numLigne),     (numColonne, numLigne),     (numColonne + 1, numLigne) ],
+  //   [ (numColonne - 1, numLigne + 1), (numColonne, numLigne + 1), (numColonne + 1, numLigne + 1) ],
+  // ]
+
+
+  function getNbVoisinsVivants ($numColonne, $numLigne, $plateau) {
+    
+    return $nbVoisinVivants;
+  }
 
   // troisième exercice : écrire une fonction qui pour une position donnée et un tableau donné 
   // retourne l'état prochain de la celulle à cette position
 
   // quatrième exercice : afficher le nouvel état du tableau
+
+
