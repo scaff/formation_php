535cfa7c933d1c778aa88002089de7612d0eac7a
Thibault Roussel <thibaultroussel@MacBook-Pro-de-Thibault-2.local>
2020-11-18 12:04:56 +0100

    ajout de getNbVoisinsVivants

--- jeu_de_la_vie.php
+++ jeu_de_la_vie.php
@@ -6,6 +6,7 @@
     array( 0, 0, 1, 0, 0 ), // 2
     array( 0, 0, 0, 0, 0 ), // 3
     array( 0, 0, 0, 0, 0 )  // 4
+                            // 5
   );
 
 
@@ -44,6 +45,7 @@
   //   [(2 , 3), (3 , 3), (4 , 3)],
   // ]
 
+  // CECI N'EST PAS DU PHP !!!!!
   // $tableau_exemple = [
   //   [ (numColonne - 1, numLigne - 1), (numColonne, numLigne - 1), (numColonne + 1, numLigne - 1) ],
   //   [ (numColonne - 1, numLigne),     (numColonne, numLigne),     (numColonne + 1, numLigne) ],
@@ -52,9 +54,31 @@
 
 
   function getNbVoisinsVivants ($numColonne, $numLigne, $plateau) {
-    
+    $nbVoisinVivants = 0;
+
+    // on parcourt la liste des lignes voisines qui vont de ligne - 1 à ligne + 1
+    for ($ligne = $numLigne - 1; $ligne <= $numLigne + 1; $ligne++) {
+      // on parcourt la liste des cellules voisines qui vont de colonne - 1 à colonne + 1
+      for ($colonne = $numColonne - 1; $colonne <= $numColonne + 1; $colonne++) {
+        // on ne compte pas la cellule du centre
+        $isCelluleDuCentre = ($ligne == $numLigne && $colonne == $numColonne);
+        
+        // on evite de sortir du tableau (si numColonne = 0 & si numColonne > taille du tableau, de même pour numLigne)
+        $isBordHaut = $numLigne <= 0;
+        $isBordGauche = $numColonne <= 0;
+        $isBordBas = $numLigne < count($plateau);
+        $isBordDroit = $numColonne < count($plateau[$ligne]);
+
+        if (!$isCelluleDuCentre || !$isBordHaut || !$isBordGauche || !$isBordDroit || !$isBordBas) {
+          $nbVoisinVivants = $nbVoisinVivants + $plateau[$ligne][$colonne];
+        }
+      }
+    }
+
     return $nbVoisinVivants;
   }
+
+  echo getNbVoisinsVivants(4, 4, $plateau);
 
   // troisième exercice : écrire une fonction qui pour une position donnée et un tableau donné 
   // retourne l'état prochain de la celulle à cette position
